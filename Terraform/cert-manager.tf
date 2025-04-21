aeb7a98ac0f26d26035b387e9d543002054f0a53
Charith Herath <64669020+Cjay13@users.noreply.github.com>
2025-04-21 07:50:16 +0000

    testing

--- Terraform/cert-manager.tf
+++ Terraform/cert-manager.tf
@@ -1,11 +1,11 @@
-module "cert-manager" {
-    depends_on = [module.eks]
-    source = "git::https://github.com/Cjay13/terraform-kubernetes-cert-manager.git?ref=main"
-    chart_version            = "v1.17.0"
-    create_clusterIssuer     = true
-    clusterIssuer_name       = "letsencrypt-prod"
-    acme_server_url          = "https://acme-v02.api.letsencrypt.org/directory"
-    letsencrypt_email        = "herath.charithjay@gmail.com"
-    private_key_secret_name  = "letsencrypt-prod"
-    ingress_class            = "nginx"
-}
+#module "cert-manager" {
+#    depends_on = [module.eks]
+#    source = "git::https://github.com/Cjay13/terraform-kubernetes-cert-manager.git?ref=main"
+#    chart_version            = "v1.17.0"
+#    create_clusterIssuer     = true
+#    clusterIssuer_name       = "letsencrypt-prod"
+#    acme_server_url          = "https://acme-v02.api.letsencrypt.org/directory"
+#    letsencrypt_email        = "herath.charithjay@gmail.com"
+#    private_key_secret_name  = "letsencrypt-prod"
+#    ingress_class            = "nginx"
+#}
