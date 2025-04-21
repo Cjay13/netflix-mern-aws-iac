9e380ef3827205b426a1a1e06e4dea1314d11118
Charith Herath <64669020+Cjay13@users.noreply.github.com>
2025-04-21 11:43:06 +0000

    testing

--- Terraform/cert-manager.tf
+++ Terraform/cert-manager.tf
@@ -1,6 +1,11 @@
 module "cert-manager" {
     depends_on = [module.eks]
     source = "git::https://github.com/Cjay13/terraform-kubernetes-cert-manager.git?ref=main"
+
+    providers = {
+        kubectl = gavinbunney/kubectl
+    }
+    
     chart_version            = "v1.17.0"
     create_clusterIssuer     = true
     clusterIssuer_name       = "letsencrypt-prod"
