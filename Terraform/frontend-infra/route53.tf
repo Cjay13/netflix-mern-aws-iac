067a3c5843b7b9062c9d6ec351eaa320ad49984d
Charith Herath <64669020+Cjay13@users.noreply.github.com>
2025-04-27 06:33:37 +0000

    updated domain validation

--- Terraform/frontend-infra/route53.tf
+++ Terraform/frontend-infra/route53.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "acm_validation" {
   zone_id = var.route53_zone_id
 
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.netflix-cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
